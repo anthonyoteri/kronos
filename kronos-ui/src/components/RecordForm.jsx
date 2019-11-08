fd07e435c70bdcc6f65bd290c3ee6b26dbc41080
Anthony Oteri <anthony.oteri@edgeware.tv>
2019-11-08 16:32:56 -0500

    Change date format

--- kronos-ui/src/components/RecordForm.jsx
+++ kronos-ui/src/components/RecordForm.jsx
@@ -73,7 +73,7 @@ class RecordForm extends Component {
     render() {
         const { visible, title, projects, onCancel } = this.props;
         const { project, startTime, stopTime } = this.state;
-        const timeFormat = 'YYYY-MM-DD HH:mm:ss';
+        const timeFormat = 'ddd MMM Do, Y h:mm A';
 
         const filteredProjects = [...projects].filter(p => !p.locked);
 
