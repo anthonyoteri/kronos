b78e95c707a978025d9299e6e7aee4b1cad1ee15
Anthony Oteri <anthony.oteri@edgeware.tv>
2019-11-12 04:18:14 -0500

    Use persistent DB connections

--- kronos-api/backend/serializers.py
+++ kronos-api/backend/serializers.py
@@ -8,11 +8,9 @@ class ProjectSerializer(serializers.ModelSerializer):
         fields = ('slug', 'name', 'description', 'locked', 'last_used', 'created', 'updated')
 
 
-
 class RecordSerializer(serializers.ModelSerializer):
     class Meta:
         model = Record
         fields = ('id', "project", "start_time", "stop_time")
 
-    
     project = serializers.SlugRelatedField(slug_field="slug", queryset=Project.objects.all())
--- kronos-api/kronos/settings.py
+++ kronos-api/kronos/settings.py
@@ -84,6 +84,7 @@ DATABASES = {
         'PASSWORD': os.environ.get("MYSQL_PASSWORD"),
         'HOST': 'db',
         'PORT': 3306,
+        'CONN_MAX_AGE': None,
     }
 }
 
